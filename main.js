300a2231694de3152d0af663677a16c41ccf693d
nati-man1 <aradomnatnael@gmail.com>
2024-11-24 12:13:57 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-$(document).ready(function() {
-    $(function () {
+$(document).ready(function () {
     var flame = $("#flame");
     var txt = $("h1");
 
-    flame.on({
-        click: function () {
+    // Function to blow out the candle
+    function blowOutCandle() {
         flame.removeClass("burn").addClass("puff");
         $(".smoke").each(function () {
             $(this).addClass("puff-bubble");
@@ -13,10 +12,45 @@ $(document).ready(function() {
         txt.hide().html("Wish you a happy birthday Maria").delay(750).fadeIn(300);
         $("#candle").animate(
             {
-            opacity: ".5"
+                opacity: ".5",
             },
             100
         );
-        }
-    });
-});});
+    }
+
+    // Set up Web Audio API for microphone input
+    navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then(function (stream) {
+            const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+            const analyser = audioContext.createAnalyser();
+            const microphone = audioContext.createMediaStreamSource(stream);
+            const dataArray = new Uint8Array(analyser.frequencyBinCount);
+
+            analyser.smoothingTimeConstant = 0.8;
+            analyser.fftSize = 1024;
+
+            microphone.connect(analyser);
+
+            // Check microphone input volume
+            function detectBlow() {
+                analyser.getByteFrequencyData(dataArray);
+
+                // Get average volume
+                const volume = dataArray.reduce((a, b) => a + b) / dataArray.length;
+
+                // If volume exceeds threshold, blow out the candle
+                if (volume > 60) {
+                    blowOutCandle();
+                }
+
+                requestAnimationFrame(detectBlow);
+            }
+
+            detectBlow(); // Start detecting sound
+        })
+        .catch(function (err) {
+            console.error("Microphone access denied:", err);
+            alert("Please allow microphone access to blow out the candle!");
+        });
+});
